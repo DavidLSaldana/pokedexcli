82396d515402fdb15acb785cc8356a5f70e0bfa4
DavidLSaldana <dleosaldana@gmail.com>
2025-03-21 07:30:46 -0700

    setting up tests

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/DavidLSaldana/pokedexcli
+
+go 1.24.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+// purpose of this Fn will be to split users text into "words"
+// based on whitespace
+func cleanInput(text string) []string {
+	strSlice := make([]string, 0)
+	return strSlice
+}
